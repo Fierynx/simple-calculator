4a207c0b4f3b7b22028b4afa922f186e6196764f
Ryu Orlando Tamin <ryutamin05@gmail.com>
2024-11-21 19:40:17 +0700

    fix: bug & enhance modularity

--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -1,6 +1,90 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
+export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
 }
+
+const infixToPrefix = (expression: string) => {
+  const operators: string[] = [];
+  const operands: string[] = [];
+  let num = "";
+  expression = expression + " ";
+
+  for (let i = expression.length - 1; i >= 0; i--) {
+    let char = expression[i];
+    char = char === "X" ? "*" : char;
+    if (char === "-" && (i === 0 || /[+\-*/]/.test(expression[i + 1]))) {
+      num = char + num; 
+    } else if( /\d/.test(char)){
+      num = char + num;
+    } else if (!isOperand(char)) {
+      if (num) {
+        operands.push(num); 
+        num = ""; 
+      }
+
+      while (operators.length && precedence(operators[operators.length - 1]) > precedence(char)) {
+        operands.push(operators.pop()!); 
+      }
+
+      operators.push(char); 
+    }
+  }
+
+  if (num)  operands.push(num);
+  while (operators.length) operands.push(operators.pop()!);
+
+  return operands.reverse();
+};
+
+
+const precedence = (operator: string) => {
+  if (operator === "+" || operator === "-") return 1;
+  if (operator === "*" || operator === "/") return 2;
+  return 0;
+};
+
+export const isOperand = (char: string) => {
+  if (char === "+" || char === "-" || char === "*" || char === "/") return false;
+  else return true;
+};
+
+export const evaluateExpression = (expression: string) => {
+  const prefix = infixToPrefix(expression);  
+  const stack: number[] = [];
+
+  for (let j = prefix.length - 1; j >= 0; j--) {
+    if(isOperand(prefix[j])) stack.push(parseFloat(prefix[j]));
+    else {
+      const o1 = stack.pop()!;
+      const o2 = stack.pop()!;
+      switch (prefix[j]) {
+        case '+':
+          stack.push(o1 + o2);
+          break;
+        case '-':
+          stack.push(o1 - o2);
+          break;
+        case '*':
+          stack.push(o1 * o2);
+          break;
+        case '/': {
+          if (o2 === 0) return "Err"; 
+          let resultDivision = o1 / o2;
+          resultDivision = parseFloat(resultDivision.toFixed(4));
+          stack.push(resultDivision);
+          break;
+        }
+      }
+    }
+  }
+
+  const result = stack[stack.length - 1];
+
+  if(result >= 1e3 || result <= -1e3) {
+    return result.toExponential(3).toString();
+  }
+
+  return result.toString();
+};
--- src/pages/Calculator.tsx
+++ src/pages/Calculator.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import DisplayScreen from "../components/calculator/DisplayScreen";
 import Keypad from "../components/calculator/Keypad";
+import { evaluateExpression, isOperand } from "../lib/utils";
 
 export default function Calculator() {
   const [input, setInput] = useState<string>("0");
@@ -36,93 +37,6 @@ export default function Calculator() {
     }else {
       setInput(input === "0" ? value : input + value); 
     }
-  };
-  
-  const infixToPrefix = (expression: string) => {
-    const operators: string[] = [];
-    const operands: string[] = [];
-    let num = "";
-    expression = expression + " ";
-  
-    for (let i = expression.length - 1; i >= 0; i--) {
-      let char = expression[i];
-      char = char === "X" ? "*" : char;
-      if (char === "-" && (i === 0 || /[+\-*/]/.test(expression[i + 1]))) {
-        num = char + num; 
-      } else if( /\d/.test(char)){
-        num = char + num;
-      } else if (!isOperand(char)) {
-        if (num) {
-          operands.push(num); 
-          num = ""; 
-        }
-  
-        while (operators.length && precedence(operators[operators.length - 1]) > precedence(char)) {
-          operands.push(operators.pop()!); 
-        }
-  
-        operators.push(char); 
-      }
-    }
-  
-    if (num)  operands.push(num);
-    while (operators.length) operands.push(operators.pop()!);
-  
-    return operands.reverse();
-  };
-  
-  
-  const precedence = (operator: string) => {
-    if (operator === "+" || operator === "-") return 1;
-    if (operator === "*" || operator === "/") return 2;
-    return 0;
-  };
-
-  const isOperand = (char: string) => {
-    if (char === "+" || char === "-" || char === "*" || char === "/") return false;
-    else return true;
-  };
-
-  const evaluateExpression = (expression: string) => {
-    const prefix = infixToPrefix(expression);  
-    const stack: number[] = [];
-  
-    for (let j = prefix.length - 1; j >= 0; j--) {
-  
-      if (isOperand(prefix[j])) stack.push(parseInt(prefix[j]));
-      else {
-        const o1 = stack.pop()!;
-        const o2 = stack.pop()!;
-  
-        switch (prefix[j]) {
-          case '+':
-            stack.push(o1 + o2);
-            break;
-          case '-':
-            stack.push(o1 - o2);
-            break;
-          case '*':
-            stack.push(o1 * o2);
-            break;
-          case '/':
-            if (o2 === 0) return "Err";
-            stack.push(o1 / o2);
-            break;
-        }
-      }
-    }
-  
-    const result = stack[stack.length - 1];
-
-    if(result >= 1e5 || result <= -1e5) {
-      return result.toExponential(3).toString();
-    }
-
-    if(result.toString().includes(".") && result.toString().split(".")[1].length > 3) {
-      return parseFloat(result.toFixed(3)).toString();
-    }
-
-    return result.toString();
   };
   
   return (
